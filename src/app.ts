b4aaf09c865eb7708bd8946a6aecd965ea94c3d3
Borislav Petrov <boris.petrov@digital.cabinet-office.gov.uk>
2022-01-20 18:38:23 +0000

    Fixed issues with assets not loading on Decide stage, removed google analytics id implementation, updated cookies bar to work with new GA tag implementation

--- src/app.ts
+++ src/app.ts
@@ -11,6 +11,7 @@ import ZendeskService from "./zendesk/ZendeskService";
 const app = express();
 const bodyParser = require('body-parser')
 
+app.use('/dist', express.static('./dist/assets'));
 app.use(express.static('./dist'));
 
 configureViews(app);
