7e462b8941c4c97892e5b9f123b631e534101f0a
Borislav Petrov <boris.petrov@digital.cabinet-office.gov.uk>
2022-05-27 12:36:02 +0100

    Async read file in mailing list controller, add valuse to env example
    Fix type in file name in mailing list controller

--- src/controllers/mailing-list.ts
+++ src/controllers/mailing-list.ts
@@ -1,7 +1,7 @@
 import express, {NextFunction, Request, Response} from 'express';
 import SheetsService from "../lib/sheets/SheetsService";
 import * as EmailValidator from 'email-validator';
-import fs from 'fs';
+import {promises as fs} from 'fs';
 
 export const mailingList = async function(req: Request, res: Response) {
   const personalName = req.body.personalName;
@@ -34,7 +34,7 @@ export const mailingList = async function(req: Request, res: Response) {
     errorMessages.set('contactEmail', 'Enter an email address in the correct format, like name@example.gov.uk');
   } else {
     try {
-      const emails = fs.readFileSync("./valid-email-domains.txt", "utf-8");
+      const emails = await fs.readFile("./valid-email-domains.txt", "utf-8");
       let validEmailDomains = [];
       validEmailDomains = emails.split("\n");
 
