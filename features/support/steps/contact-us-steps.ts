ec500c359d0e24847eddc0936facd03cb8cbc497
Karol C. Gancarz <karol.sinx@gmail.com>
2022-07-27 11:54:23 +0100

    BAU: Correctly construct URLs in feature tests
    Avoid failing tests due to double slashes caused by directly adding two strings. Use the URL class instead to handle URL construction.
    
    Remove duplicated code fragments by moving the repeated blocks into functions.

--- features/support/steps/contact-us-steps.ts
+++ features/support/steps/contact-us-steps.ts
@@ -1,13 +1,9 @@
 import {Given, Then} from "@cucumber/cucumber";
 
 Given('that the users enter alphanumeric characters into all of the fields in the contact us form', async function () {
-    await this.goToPath("/contact-us");
-    await this.page.type('#email', 'tessa.ting@foo-bar.gov.uk');
-    await this.page.type("#name", 'Tessa Ting');
-    await this.page.type("#role", 'Chief Unicorn Tester');
-    await this.page.type("#service-name", "Unicorn Testing");
-    await this.page.type("#organisation-name", "Department of Sorcery");
-    await this.page.type("#how-can-we-help", "We'd like fairies to be able to sign up to get their unicorns tested.");
+    await this.goToPath('/contact-us');
+    await fillInUserInfoFields(this.page);
+    await this.page.type('#how-can-we-help', "We'd like fairies to be able to sign up to get their unicorns tested.");
 });
 
 Given('that the users enter alphanumeric characters into all of the fields in the contact us form except the name field', async function () {
@@ -55,12 +51,8 @@ Given('that the users enter alphanumeric characters into all of the fields in th
 });
 
 Given('that the users enter alphanumeric characters into all of the fields in the contact us form except the how-can-we-help field', async function () {
-    await this.goToPath("/contact-us");
-    await this.page.type('#email', 'tessa.ting@foo-bar.gov.uk');
-    await this.page.type("#name", 'Tessa Ting');
-    await this.page.type("#role", 'Chief Unicorn Tester');
-    await this.page.type("#service-name", "Unicorn Testing");
-    await this.page.type("#organisation-name", "Department of Sorcery");
+    await this.goToPath('/contact-us');
+    await fillInUserInfoFields(this.page);
 });
 
 Given('that the user enters an invalid email address into the email field of the contact us form', async function () {
@@ -77,3 +69,11 @@ Then('their data is sent to Zendesk', async function () {
     // We can't really test this unless we just run a unit test or something
     return true;
 });
+
+async function fillInUserInfoFields(page: Page) {
+    await page.type('#email', 'tessa.ting@foo-bar.gov.uk');
+    await page.type('#name', 'Tessa Ting');
+    await page.type('#role', 'Chief Unicorn Tester');
+    await page.type('#service-name', 'Unicorn Testing');
+    await page.type('#organisation-name', 'Department of Sorcery');
+}
