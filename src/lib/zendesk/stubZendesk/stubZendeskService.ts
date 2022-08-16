302aec5d30680a6b103dd16b2f9121681aacb8c6
Karol C. Gancarz <karol.sinx@gmail.com>
2022-08-16 16:19:14 +0100

    BAU: Enable the noUnusedLocals flag in tsconfig.json
    Remove unused locals to let the build succeed

--- src/lib/zendesk/stubZendesk/stubZendeskService.ts
+++ src/lib/zendesk/stubZendesk/stubZendeskService.ts
@@ -1,18 +1,7 @@
 import ZendeskInterface from "../interface";
 
 export default class StubZendeskService implements ZendeskInterface {
-    private email: string;
-    private apiToken: string;
-    private tag: string;
-
-    constructor(email: string, apiToken: string, tag: string) {
-        this.email = email;
-        this.apiToken = apiToken;
-        this.tag = tag;
-    }
-
-
-    submit(form:  Map<string, string>): Promise<boolean> {
+    submit(form: Map<string, string>): Promise<boolean> {
         return Promise.resolve(true);
     }
 }
