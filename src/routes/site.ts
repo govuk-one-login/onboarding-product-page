ceaaf12006df4ad8e6ac501a62d2329c64e1abbf
bethwalton <beth.walton@digital.cabinet-office.gov.uk>
2023-02-17 12:49:46 +0000

    Add international phone numbers updates

--- src/routes/site.ts
+++ src/routes/site.ts
@@ -44,13 +44,15 @@ router.get("/documentation/user-journeys", (req, res) => {
     res.render("documentation-user-journeys.njk");
 });
 
-router.get("/users-create-an-account-upfront-pdf-november-2022", (req, res) => {
-    res.sendFile(path.join(resources, "journeys/GOVUK_One_Login_user_journeys_Option_1_Users_create_an_account_upfront_11-2022.pdf"));
+router.get("/users-create-an-account-upfront-pdf-february-2023", (req, res) => {
+    res.sendFile(
+        path.join(resources, "journeys/GOVUK_One_Login_user_journeys_Option_1_Users_create_a_GOVUK_One_Login_upfront_02_2023.pdf")
+    );
 });
 
-router.get("/users-create-an-account-to-save-progress-pdf-november-2022", (req, res) => {
+router.get("/users-create-an-account-to-save-progress-pdf-february-2023", (req, res) => {
     res.sendFile(
-        path.join(resources, "journeys/GOVUK_One_Login_user_journeys_Option_2_Users_create_an_account_to_save_progress_11-2022.pdf")
+        path.join(resources, "journeys/GOVUK_One_Login_user_journeys_Option_2_Users_create_a_GOVUK_One_Login_to_save_progress_02_2023.pdf")
     );
 });
 
