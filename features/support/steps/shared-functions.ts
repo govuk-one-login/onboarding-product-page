66dc4ff1d15009ab53a14fc9f3c634ed662dfd15
Borislav Petrov <boris.petrov@digital.cabinet-office.gov.uk>
2023-08-16 16:30:23 +0100

    SSE-2787 - Connect Register your interest form to google spreadsheet, map values

--- features/support/steps/shared-functions.ts
+++ features/support/steps/shared-functions.ts
@@ -1,6 +1,8 @@
 import {strict as assert} from "assert";
 import {ElementHandle, Page} from "puppeteer";
 
+const defaultTimeout = 5000;
+// eslint-disable-next-line
 export async function getLink(page: Page, linkText: string): Promise<any> {
     const links = await page.$x(`//a[text()="${linkText}"]`);
     assert.equal(links.length, 1, `More than one link matched ${linkText}`);
@@ -9,7 +11,44 @@ export async function getLink(page: Page, linkText: string): Promise<any> {
 
 export async function checkUrl(page: Page, links: ElementHandle[], expectedUrl: string): Promise<void> {
     assert.equal(
+        // eslint-disable-next-line
         await page.evaluate((anchor: {getAttribute: (arg0: string) => any}) => anchor.getAttribute("href"), links[0]),
         expectedUrl
+    );
+}
+
+export async function enterTextIntoTextInput(page: Page, text: string, inputId: string) {
+    const inputElement = await page.$(`#${inputId}`);
+    if (!inputElement) {
+        throw new Error(`Could not find element with id ${inputId}`);
+    }
+
+    await inputElement.click({clickCount: 3});
+    await inputElement.press("Backspace");
+    await page.type(`#${inputId}`, text);
+}
+
+export async function clickButtonWithId(page: Page, id: string, timeout = defaultTimeout) {
+    await Promise.all([page.waitForNavigation({timeout: timeout}), page.click(`#${id}`)]);
+}
+
+export async function clickSubmitButton(page: Page, timeout = defaultTimeout) {
+    await clickButtonWithId(page, "submit", timeout);
+}
+
+export async function checkErrorMessageDisplayedForField(page: Page, errorLink: ElementHandle[], errorMessage: string, field: string) {
+    assert.notEqual(errorLink.length, 0, `Expected to find the message ${errorMessage} in the error summary.`);
+
+    const messageInSummary = await page.evaluate(element => element.textContent, errorLink[0]);
+    assert.equal(messageInSummary, errorMessage, `Expected text of the link to be ${errorMessage}`);
+
+    const messagesAboveElement = await page.$x(`//p[@class="govuk-error-message"][@id="${field}-error"]`);
+    assert.notEqual(messagesAboveElement.length, 0, `Expected to find the message ${errorMessage} above the ${field} field.`);
+
+    const messageAboveElement = await page.evaluate(element => element.textContent, messagesAboveElement[0]);
+    assert.equal(
+        messageAboveElement?.trim(),
+        "Error: " + errorMessage,
+        `Expected the message above the ${field} field to be ${errorMessage}`
     );
 }
