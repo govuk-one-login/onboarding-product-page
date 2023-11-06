c3e26025665cb96dd0eaf2739585b9a47699d5c9
pradeepkumar22202 <pradeep.kumar@digital.cabinet-office.gov.uk>
2023-11-06 08:55:49 +0000

    main code for PaaS

---
+++ infrastructure/deploy-sam-stack.sh
@@ -0,0 +1,108 @@
+#!/usr/bin/env bash
+# Utility script to deploy SAM stacks
+BASE_DIR="$(dirname "${BASH_SOURCE[0]}")"
+set -e
+
+OPTION_REGEX="^--?.*"
+BUILD=false
+DEPLOY=true
+
+while [[ -n $1 ]]; do
+  case $1 in
+    -n | --stack-name)
+      [[ -z ${2:-} || $2 =~ $OPTION_REGEX ]] && echo "Invalid stack name: '$2'" && exit 1
+      shift && STACK_NAME=$1
+      ;;
+    -t | --tags)
+      while [[ $2 ]] && ! [[ $2 =~ $OPTION_REGEX ]]; do
+        shift && TAGS+=("$1")
+      done
+      ;;
+    -o | --params | --parameters | --parameter-overrides)
+      while [[ $2 ]] && ! [[ $2 =~ $OPTION_REGEX ]]; do
+        shift && PARAMS+=("$1")
+      done
+      ;;
+    -s3 | --prefix | --s3-prefix) shift && S3_PREFIX=$1 ;;
+    -f | --template | --template-file) shift && TEMPLATE=$1 && VALIDATE=true ;;
+    -a | --account) shift && ACCOUNT=$1 ;;
+    -s | --base-dir) shift && SAM_BASE_DIR=$1 ;;
+    -b | --build) BUILD=true ;;
+    -d | --no-build) SKIP_BUILD=true ;;
+    -x | --no-deploy) DEPLOY=false ;;
+    -c | --cached) CACHED=true ;;
+    -p | --parallel) PARALLEL=true ;;
+    -y | --no-confirm) NO_CONFIRM=true ;;
+    -r | --disable-rollback) DISABLE_ROLLBACK=true ;;
+    -v | --validate) VALIDATE=true ;;
+    -g | --guided) GUIDED=true ;;
+    --config-file) shift && CONFIG_FILE=$1 ;;
+    --config-env) shift && CONFIG_ENV=$1 ;;
+    --force-upload) FORCE_UPLOAD=true ;;
+    --no-delete-rollback-complete) DELETE_ON_FAILED_CREATION=false ;;
+    *) echo "Unknown option '$1'" && exit 1 ;;
+  esac
+  shift
+done
+
+$DEPLOY && ! "$BASE_DIR"/aws.sh check-current-account "${ACCOUNT:-}" 2> /dev/null &&
+  echo "Authenticate to${ACCOUNT:+" the '$ACCOUNT' account in"} AWS before deploying the stack" && exit 1
+
+[[ $TEMPLATE ]] && ! [[ -f $TEMPLATE ]] && echo "File '$TEMPLATE' does not exist" && exit 1
+${SKIP_BUILD:-false} && BUILD=false
+
+if $DEPLOY; then
+  [[ -f samconfig.toml || -f $CONFIG_FILE ]] && CONFIG=true
+
+  if [[ $STACK_NAME || $CONFIG || $GUIDED ]]; then
+    TAGS+=(sse:application=self-service sse:deployment-source=manual)
+    : "${S3_PREFIX:=$STACK_NAME}"
+
+    echo "Deploying${STACK_NAME:+" stack '$STACK_NAME'"}${TEMPLATE:+" from template '$TEMPLATE'"}"
+    [[ $S3_PREFIX ]] && echo "  with S3 prefix '$S3_PREFIX'"
+    [[ ${TAGS[*]} ]] && echo "  with tags '${TAGS[*]}'"
+    [[ ${PARAMS[*]} ]] && echo "  with params '${PARAMS[*]}'"
+  else
+    echo "𝙭 Stack name not provided - not deploying (use the --stack-name|-n or --guided option)" && DEPLOY=false
+  fi
+fi
+
+if $BUILD || ${VALIDATE:-false}; then
+  echo "Validating template..."
+  sam validate ${TEMPLATE:+--template "$TEMPLATE"}
+  sam validate ${TEMPLATE:+--template "$TEMPLATE"} --lint
+fi
+
+if $BUILD; then
+  echo "Building${TEMPLATE:+" template '$TEMPLATE'"}..."
+  sam build \
+    ${TEMPLATE:+--template $TEMPLATE} \
+    ${SAM_BASE_DIR:+--base-dir $SAM_BASE_DIR} \
+    ${PARALLEL:+--parallel} \
+    ${CACHED:+--cached}
+fi
+
+$DEPLOY || exit 0
+$BUILD || [[ -e .aws-sam/build/template.yaml ]] && unset TEMPLATE
+${NO_CONFIRM:-false} && CONFIRM_OPTION="--no-confirm-changeset"
+${DISABLE_ROLLBACK:-false} && DISABLE_ROLLBACK_OPTION="--disable-rollback"
+
+[[ $(aws cloudformation describe-stacks --stack-name "$STACK_NAME" --query "Stacks[].StackStatus" \
+  --output text 2> /dev/null) == ROLLBACK_COMPLETE ]] && ${DELETE_ON_FAILED_CREATION:-true} &&
+  sam delete --no-prompts --region "${AWS_REGION:-${AWS_DEFAULT_REGION}}" --stack-name "$STACK_NAME"
+
+sam deploy \
+  ${STACK_NAME:+--stack-name "$STACK_NAME"} \
+  ${S3_PREFIX:+--s3-prefix "$S3_PREFIX"} \
+  ${CONFIG:---resolve-s3} \
+  ${DISABLE_ROLLBACK_OPTION:---no-disable-rollback} \
+  --no-fail-on-empty-changeset \
+  --capabilities CAPABILITY_IAM CAPABILITY_NAMED_IAM CAPABILITY_AUTO_EXPAND \
+  ${CONFIRM_OPTION:---confirm-changeset} \
+  ${TEMPLATE:+--template "$TEMPLATE"} \
+  ${FORCE_UPLOAD:+--force-upload} \
+  ${TAGS:+--tags "${TAGS[@]}"} \
+  #${PARAMS:+--parameter-overrides "${PARAMS[@]}"} \
+  ${CONFIG_FILE:+--config-file "$CONFIG_FILE"} \
+  ${CONFIG_ENV:+--config-env "$CONFIG_ENV"} \
+  ${GUIDED:+--guided}
---
+++ infrastructure/dev/deploy.sh
@@ -0,0 +1,182 @@
+#!/usr/bin/env bash
+# The "it just works" script to deploy backend stacks and run a local Admin Tool frontend against them
+cd "$(dirname "${BASH_SOURCE[0]}")"
+set -eu -o pipefail
+
+
+
+COMPONENTS=(frontend cognito dynamodb api)
+DEPLOY_CMDS=("${COMPONENTS[@]}" all admin-tool)
+COMMANDS=("${DEPLOY_CMDS[@]}" run open list exports delete help)
+
+../aws.sh check-current-account development &> /dev/null || eval "$(gds aws di-onboarding-development -e)"
+USER_NAME=$(../aws.sh get-user-name)
+ECR_REPO=self-service/frontend
+LOG_PREFIX=/self-service/dev
+REPO_ROOT=$(pwd)/../..
+OPTION_REGEX="^--?.*"
+DEV_PREFIX=product
+
+
+
+
+
+function help {
+  (IFS="|" && echo "deploy.sh [${COMMANDS[*]}] [prefix] [SAM deploy options]") && exit
+}
+
+function get-export {
+  jq --exit-status --raw-output --arg name "$STACK_PREFIX-$1" '.[] | select(.Name == $name) | .Value' <<< "$exports"
+}
+
+function get-secret {
+  aws secretsmanager get-secret-value --secret-id "/self-service/$1" --query SecretString --output text
+}
+
+function set-prefix {
+  [[ ${STACK_PREFIX:-} ]] && return || STACK_PREFIX=${1:-}
+
+  if ! [[ ${STACK_PREFIX:-} ]]; then
+    STACK_PREFIX=$(tr "." "-" <<< "$USER_NAME")
+    echo "𝓲 Stack prefix not set; using the default prefix '$STACK_PREFIX'" >&2
+  fi
+
+  STACK_PREFIX=$DEV_PREFIX-${STACK_PREFIX##"${DEV_PREFIX}"-}
+}
+
+function get-env-vars {
+  local exports var secret
+  exports=$(aws cloudformation list-exports --query "Exports[?starts_with(Name, '$STACK_PREFIX-')]")
+  [[ $exports == "[]" ]] && echo "No exports found for stack prefix '$STACK_PREFIX'" && exit
+
+  for var in "${!ENV[@]}"; do
+    env+=("$var=$(get-export "${ENV[$var]}")") && continue
+    echo "𝙭 Export '${ENV[$var]}' not found" >&2 && return 1
+  done
+
+  for secret in "${!SECRETS[@]}"; do
+    env+=("$secret=$(get-secret "${SECRETS[$secret]}")") && continue
+    echo "𝙭 Secret '${SECRETS[$secret]}' not found" >&2 && return 1
+  done
+}
+
+function exports {
+  get-env-vars
+  (IFS=$'\n' && echo "${env[*]}")
+}
+
+
+function delete {
+  local stack
+  for stack in $(list); do
+    sam delete --no-prompts --region "${AWS_REGION:-${AWS_DEFAULT_REGION}}" --stack-name "$stack"
+  done
+}
+
+function deploy-backend-component {
+  local component=$1
+  deploy "$REPO_ROOT"/backend/"$component" "$component" "${@:2}"
+}
+
+function deploy {
+  local dir=$1 component=$2 template
+  pushd "$dir" > /dev/null
+  OPTIONS=[]
+
+  [[ ${OPTIONS[*]} =~ --template|--template-file|-f ]] || template=$component.template.yml
+
+  "$REPO_ROOT"/infrastructure/deploy-sam-stack.sh "${@:3}" "${OPTIONS[@]}" \
+    --account development \
+    ${template:+--template $template} \
+    ${STACK_PREFIX:+--stack-name $STACK_PREFIX-$component} \
+    --tags sse:component="$component" sse:stack-type=dev sse:stack-role=application sse:owner="$USER_NAME" \
+    --params ${STACK_PREFIX:+DeploymentName=$STACK_PREFIX} LogGroupPrefix=$LOG_PREFIX
+
+  popd > /dev/null
+}
+
+function build-frontend-image {
+  local branch commit repo image
+
+  repo=$(aws ecr describe-repositories --repository-names $ECR_REPO --output text --query "repositories[0].repositoryUri")
+  commit=$(git rev-parse HEAD)
+  image_uri="$repo:$commit"
+
+  image=$(aws ecr list-images --repository-name $ECR_REPO --query "imageIds[?imageTag=='$commit']")
+  [[ $image != "[]" ]] && echo "Image for commit $commit is present in ECR" && return 0
+
+  branch=$(git branch --show-current)
+  image=$(aws ecr describe-images --repository-name $ECR_REPO --image-ids imageTag="$branch" \
+    --query imageDetails[0].imageDigest --output text 2> /dev/null) &&
+    aws ecr batch-delete-image --repository-name $ECR_REPO --image-ids imageDigest="$image" > /dev/null &&
+    echo "» Deleted image for branch $branch"
+
+  echo "» Building frontend"
+  npm run build --include-workspace-root &> /dev/null
+
+  echo "» Building Docker image"
+  docker build --quiet --platform linux/amd64 --tag "$repo:$branch" --tag "$image_uri" \
+    --file /Users/pradeep.kumar/di-onboarding-product-page/Dockerfile "$REPO_ROOT"
+
+  echo "» Pushing image to ECR"
+  docker push --quiet "$repo:$branch" && docker push --quiet "$image_uri"
+}
+
+function run {
+  get-env-vars && (IFS=$'\n' && echo "${env[*]}") || exit
+  eval "${env[*]}" STUB_API=false npm run dev
+}
+
+function open {
+  local url
+  [[ $STACK_PREFIX ]] || exit
+  url=$(../aws.sh get-stack-outputs "$STACK_PREFIX"-frontend AdminToolURL | jq --raw-output .value) &&
+    echo "Opening $url..." && command open https://"${url##https://}" && exit ||
+    echo "𝙭 Remote frontend not deployed for prefix '${STACK_PREFIX##"${DEV_PREFIX}"-}'" && exit 1
+}
+
+function frontend {
+
+
+  build-frontend-image
+  deploy "$REPO_ROOT"/infrastructure/frontend frontend --params ImageURI="$image_uri"
+
+  if [[ $STACK_PREFIX ]]; then
+    echo
+  fi
+
+  open
+
+}
+
+function dynamodb {
+  deploy-backend-component dynamodb
+}
+
+function cognito {
+  deploy-backend-component cognito --build
+}
+
+function api {
+  deploy-backend-component api --build --base-dir "$REPO_ROOT" --params PrivateAPI=false
+}
+
+function all {
+  echo "Deploying the Admin Tool stack set"
+  #dynamodb
+  #cognito
+  #api
+  frontend
+}
+
+function admin-tool {
+  all
+  run
+}
+
+[[ ${COMMANDS[*]} =~ ${1:-} ]] && COMMAND=$1 && shift || COMMAND=all
+[[ ${1:--} =~ $OPTION_REGEX ]] || { set-prefix "$1" && shift; }
+
+OPTIONS=("$@")
+set-prefix
+$COMMAND
