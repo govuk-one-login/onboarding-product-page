4844383c89f17a0b0cec0e06ab7384e91d5ad8ad
Huw Diprose <huw.diprose@digital.cabinet-office.gov.uk>
2024-03-12 16:19:30 +0000

    SSE-3210 Add customfield_11542 total annual users
    Co-authored-by: Ryan Andrews <ryan.andrews@digital.cabinet-office.gov.uk>
    Co-authored-by: Huw Diprose <huw.diprose@digital.cabinet-office.gov.uk>

--- src/lib/jira/interface.ts
+++ src/lib/jira/interface.ts
@@ -7,3 +7,32 @@ export type JiraPostResponse = {
     key: string;
     self: string;
 };
+export type JiraCustomFieldPayload = JiraCustomFieldChoice;
+
+export type JiraCustomFieldChoice = {
+    self: string;
+    value: string | undefined;
+    id: string;
+};
+
+export type JiraTicketContentSection = {
+    type: string;
+    attrs?: {
+        level: number;
+    };
+    content?: JiraTicketContentPayload[];
+};
+
+type JiraTicketContentPayload = {
+    type: string;
+    text?: string | undefined;
+    marks?: JiraTicketContentPayloadMarks[];
+};
+
+type JiraTicketContentPayloadMarks = {
+    type: string;
+    attrs?: {
+        href?: string;
+    };
+};
+
