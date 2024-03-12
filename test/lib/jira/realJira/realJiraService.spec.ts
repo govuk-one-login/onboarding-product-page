08cb15f0ac3aaf44cf3ffb7042e61017271e5f52
Huw Diprose <huw.diprose@digital.cabinet-office.gov.uk>
2024-03-12 15:34:59 +0000

    SSE-3210 Create RealJiraService and validate Env Vars
    Create a basic RealJiraService class, populate key internal state
    from Env Vars, as these are external to the application and could be
    missing, let's throw an error noisily if anything we require is missing.
    
    The list of required things are:
    
    - We need a Jira username
    - We need a Jira API key
    
    Co-authored-by: Ryan Andrews <ryan.andrews@digital.cabinet-office.gov.uk>
    Co-authored-by: Huw Diprose <huw.diprose@digital.cabinet-office.gov.uk>

---
+++ test/lib/jira/realJira/realJiraService.spec.ts
@@ -0,0 +1,107 @@
+import axios from "axios";
+import sinon from "sinon";
+import {JIRA_BOARD_URL, JIRA_ISSUE_TYPE, JIRA_PROJECT_KEY} from "../../../../src/config/jiraConfig";
+import RealJiraService from "../../../../src/lib/jira/realJira/realJiraService";
+import {testApiKey, testFormSubmission, testUserName} from "../testConstants";
+
+const testFormattedJiraPayload = {
+    fields: {
+        issuetype: {
+            id: JIRA_ISSUE_TYPE
+        },
+        project: {
+            key: JIRA_PROJECT_KEY
+        },
+        summary: "Government Coffee Service: CoffeeTest",
+        description: {
+            version: 1,
+            type: "doc",
+            content: [
+                {
+                    type: "paragraph",
+                    content: [
+                        {
+                            type: "text",
+                            text: "A user has registered their interest in GOV.UK One Login"
+                        }
+                    ]
+                },
+                {
+                    type: "heading",
+                    attrs: {level: 3},
+                    content: [{type: "text", text: "Initial Contact Details"}]
+                },
+                {
+                    type: "paragraph",
+                    content: [
+                        {type: "text", text: "First Name: TestUserFirstName"},
+                        {type: "hardBreak"},
+                        {type: "text", text: "Last Name: TestUserLastName"},
+                        {type: "hardBreak"},
+                        {type: "text", text: "Email: some.TestUser@GovCoffee.gov.uk"},
+                        {
+                            type: "text",
+                            text: "some.TestUser@GovCoffee.gov.uk",
+                            marks: [{type: "link", attrs: {href: "mailto:some.TestUser@GovCoffee.gov.uk"}}]
+                        },
+                        {type: "hardBreak"},
+                        {type: "text", text: "Role: Chief Coffee Tester"}
+                    ]
+                },
+                {
+                    type: "heading",
+                    attrs: {level: 3},
+                    content: [{type: "text", text: "Contact Preferences"}]
+                },
+                {
+                    type: "paragraph",
+                    content: [
+                        {type: "text", text: "Add to mailing list: Yes ✅"},
+                        {type: "hardBreak"},
+                        {type: "text", text: "Has other services to discuss: Yes ✅"}
+                    ]
+                }
+            ]
+        }
+    }
+};
+
+let axiosStub: sinon.SinonStub;
+
+describe("realJiraService tests", () => {
+    beforeEach(() => {
+        sinon.createSandbox();
+        axiosStub = sinon.stub(axios, "post");
+        axiosStub.resolves({
+            data: {
+                id: "12345",
+                key: "testKey",
+                self: "https://testboard.atlassian.net/rest/api/3/issue/12345"
+            }
+        });
+
+        process.env.JIRA_USER_NAME = testUserName;
+        process.env.JIRA_API_KEY = testApiKey;
+    });
+
+    afterEach(() => {
+        sinon.restore();
+        delete process.env.JIRA_USER_NAME;
+        delete process.env.JIRA_API_KEY;
+    });
+
+    it("posts a ticket to the board url when postJiraTicket is called", async () => {
+        const jiraService = new RealJiraService();
+        jiraService.postJiraTicket(testFormSubmission);
+        sinon.assert.calledWith(axiosStub, JIRA_BOARD_URL, JSON.stringify(testFormattedJiraPayload), {
+            headers: {
+                "Content-Type": "application/json",
+                Accept: "application/json"
+            },
+            auth: {
+                username: testUserName,
+                password: testApiKey
+            }
+        });
+    });
+});
