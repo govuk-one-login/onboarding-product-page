6164797ec2569a13dc6536c83df7188d21172f6d
Huw Diprose <huw.diprose@digital.cabinet-office.gov.uk>
2024-03-12 14:58:00 +0000

    SSE-3210 Create stubJiraService
    We're following the existing model here set out in the repo, we'll make
    a stub class that we can call on selectively to stub out calls that
    would have otherwise hit a real Jira instance.
    
    Also shows the basic shape of what we're working towards with the real
    service, a class that akes in the board URL and a project key, and has a
    single function to POST the result, returning a small confirmation
    payload to get logged.
    
    Co-authored-by: Ryan Andrews <ryan.andrews@digital.cabinet-office.gov.uk>
    Co-authored-by: Huw Diprose <huw.diprose@digital.cabinet-office.gov.uk>

---
+++ src/lib/jira/stubJira/stubJiraService.ts
@@ -0,0 +1,23 @@
+import {randomUUID} from "crypto";
+import {JiraPostResponse, JiraService} from "../interface";
+
+export default class StubJiraService implements JiraService {
+    private readonly jiraBoardUrl: string;
+    private readonly projectKey: string;
+
+    constructor(boardUrl: string, projectKey: string) {
+        this.jiraBoardUrl = boardUrl;
+        this.projectKey = projectKey;
+    }
+
+    async postJiraTicket(ticketPayload: Map<string, string>): Promise<JiraPostResponse> {
+        console.log(`Pretending to post Jira ticket to boardUrl: '${this.jiraBoardUrl}'`);
+        console.log("Ticket payload: ", ticketPayload);
+        const ticketId = randomUUID();
+        return {
+            id: ticketId,
+            key: this.projectKey,
+            self: `${this.jiraBoardUrl}/${ticketId}`
+        };
+    }
+}
