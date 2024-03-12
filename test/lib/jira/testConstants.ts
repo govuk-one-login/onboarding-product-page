d0680b6b862c083ceff71c2b48e8bff05a441451
Huw Diprose <huw.diprose@digital.cabinet-office.gov.uk>
2024-03-12 14:55:03 +0000

    SSE-3210 Create test constraints for Reg Interest Jira Form
    We are going to add a Jira integration to this repository so we can
    connect our external register your interest form to the internal
    Onboarding Team Practices (which are heavily Jira based). This is both
    something new, but also a swap out for similar existing processes and
    code that sits in this repo.
    
    We currenty POST the result to a google sheet. Jira is the replacement
    for that, though the two will live side by side for a bit.
    
    Star that process by creating some test data that represents both the
    kinds of submissions we get off the form, and also some of the key data
    we'd need to populate a Jira ticket
    
    Co-authored-by: Ryan Andrews <ryan.andrews@digital.cabinet-office.gov.uk>
    Co-authored-by: Huw Diprose <huw.diprose@digital.cabinet-office.gov.uk>

---
+++ test/lib/jira/testConstants.ts
@@ -0,0 +1,28 @@
+export const testFormSubmission = new Map<string, string>(
+    Object.entries({
+        firstName: "TestUserFirstName",
+        lastName: "TestUserLastName",
+        email: "some.TestUser@GovCoffee.gov.uk",
+        role: "Chief Coffee Tester",
+        organisationName: "Government Coffee Service",
+        organisationType: "Government department or Ministry",
+        serviceName: "CoffeeTest",
+        serviceDescription: "Great coffee sent direct to you",
+        linkToYourService: "https://test.coffee.gov.uk",
+        totalAnnualNumberOfUsersOfYourService: "Over 1 million users",
+        estimatedServiceGoLiveDate: "June 2026",
+        accessAndTest: "auth only",
+        likeHelpWith: "I'd like help with some other thing too",
+        anyOtherServicesToTalkAbout: "Yes",
+        getUpdatesAboutOneLogin: "YES",
+        helpWith: "gettingAccess,havingTechnicalDiscussion,walkingThrough",
+        gettingAccess: "Access to integration",
+        havingTechnicalDiscussion: "Technical discussion",
+        walkingThrough: "Onboarding walkthrough",
+        id: "6eae99dc-b728-4a65-add2-1c68271e0e8e",
+        "submission-date": "2024-02-29T10:42:12.830"
+    })
+);
+
+export const testUserName = "JiraUser";
+export const testApiKey = "JiraPW";
