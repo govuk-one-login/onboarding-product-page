17aac4348573862ac218449409046c5e9b76fc6b
Andrew Hughes-Onslow <andrew.hughes-onslow@digital.cabinet-office.gov.uk>
2024-06-14 09:27:15 +0100

    SSE-3445: Updated run test scripts.

--- ui-automation-tests/run-tests.sh
+++ ui-automation-tests/run-tests.sh
@@ -3,38 +3,43 @@
 set -eu
 set -o pipefail
 
-cat << 'EOF'
+cat <<EOF
 This script runs internal integration tests for the Product Pages application.
 
 Requirements:
-  - All stack outputs available in environment variables named: $CFN_<OutputKey>
-  - The reports directory available in environment variable:    $TEST_REPORT_DIR
+  - All stack outputs available in environment variables named: \$CFN_<OutputKey>
+  - The reports directory available in environment variable:    \$TEST_REPORT_ABSOLUTE_DIR
   - The test report dir is located at the root of the container
+
+aws-cli version: $(aws --version)
+node version: $(node --version)
+
 EOF
 
+export ENDPOINT=${CFN_ProductToolURL:-} # Set the variable for the endpoint
 export ENVIRONMENT=${TEST_ENVIRONMENT:-}
-export ENDPOINT=$CFN_ProductToolURL
 
 cd /app
 
-echo "Running heartbeat check..."
+printf "Running heartbeat check...\n"
 declare status_code
 
 # shellcheck disable=SC2154
-status_code="$(curl --silent --output /dev/null --write-out '%{http_code}' "$ENDPOINT")"
+status_code="$(curl --silent --location --output /dev/null --write-out '%{http_code}' "$ENDPOINT")"
 if [[ $status_code != "200" ]]; then
   exit 1
 fi
+printf "Endpoint ok\n"
 
-echo "Running e2e tests..."
+printf "Running e2e tests...\n"
 declare error_code=0
 
-#if [[ $TEST_ENVIRONMENT =~ development ]]; then
-  export HOST="$CFN_ProductToolURL"
+if [[ $ENVIRONMENT =~ dev ]] || [[ $ENVIRONMENT =~ build ]]; then
+  export HOST="$ENDPOINT"
   npm run acceptance-tests
-#  error_code=$?
-#
-#  cp -rf results $TEST_REPORT_ABSOLUTE_DIR
-#fi
+  error_code=$?
+
+  cp -rf reports/cucumber-report.json $TEST_REPORT_ABSOLUTE_DIR
+fi
 
 exit $error_code
