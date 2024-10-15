26e8657f8b49cf586078dc8ecdb602bd6148d475
Paul Dougan <34273121+pauldougan@users.noreply.github.com>
2024-10-15 13:43:15 +0100

    add new content for "Prepare your users to move to GOV.UK One Login"

--- src/routes/site.ts
+++ src/routes/site.ts
@@ -123,6 +123,10 @@ router.get("/documentation/design-recommendations/how-to-talk-about", (req, res)
     res.render("documentation-how-to-talk-about.njk");
 });
 
+router.get("/documentation/design-recommendations/prepare-move-content", (req, res) => {
+    res.render("documentation-prepare-move-content.njk");
+});
+
 router.get("/documentation/design-recommendations/start-page", (req, res) => {
     res.render("documentation-start-page.njk");
 });
