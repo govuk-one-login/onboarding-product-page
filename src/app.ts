fb268cb160d60b686f40fa050508f856c7af1170
Ryan Andrews <ryan.andrews@digital.cabinet-office.gov.uk>
2026-02-02 16:05:40 +0000

    BAU: Graceful shutdown [deploy]
    Ensure we close the server on SIGTERM, this way the server stops
    accepting new requests and gracefully closes

--- src/app.ts
+++ src/app.ts
@@ -66,4 +66,11 @@ app.use((err: unknown, req: Request, res: Response, next: NextFunction) => {
 });
 
 const port = process.env.PORT || 3000;
-app.listen(port, () => console.log(`Server running; listening on port ${port}`));
+const server = app.listen(port, () => console.log(`Server running; listening on port ${port}`));
+
+process.on("SIGTERM", () => {
+    console.debug("Server shut down signal received");
+    server.close(() => {
+        console.debug("Server shut down");
+    });
+});
