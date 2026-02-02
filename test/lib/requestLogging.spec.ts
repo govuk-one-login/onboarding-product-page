b62ca7d747777d4e98ac74fb719848228aefe3a8
Alex Whitehead-Smith <alex.whitehead-smith@digital.cabinet-office.gov.uk>
2026-02-02 16:24:02 +0000

    BAU: Use a stream to capture the test log events
    This means we can check the contents of the logs match what we expect.

--- test/lib/requestLogging.spec.ts
+++ test/lib/requestLogging.spec.ts
@@ -2,19 +2,32 @@ import {expect} from "chai";
 import express from "express";
 import request from "supertest";
 import pinoHttp from "pino-http";
-import logger from "../../src/lib/logger";
+import pino from "pino";
+import {Writable} from "stream";
 
 describe("Request Logging Middleware", () => {
-    it("should integrate pino-http middleware with logger", async () => {
-        const app = express();
-        app.use(pinoHttp({logger}));
-        app.get("/test", (req, res) => {
-            expect(req.log).to.exist;
-            expect(req.log.info).to.be.a("function");
-            res.status(200).send("OK");
+    it("should log HTTP requests with structured data", async () => {
+        const logs: any[] = [];
+        const stream = new Writable({
+            write(chunk, encoding, callback) {
+                logs.push(JSON.parse(chunk.toString()));
+                callback();
+            }
         });
 
-        const response = await request(app).get("/test").set("User-Agent", "test-agent");
-        expect(response.status).to.equal(200);
+        const testLogger = pino(stream);
+        const app = express();
+        app.use(pinoHttp({logger: testLogger}));
+        app.get("/test", (req, res) => res.status(200).send("OK"));
+
+        await request(app).get("/test").set("User-Agent", "test-agent");
+
+        expect(logs.length).to.be.greaterThan(0);
+        const requestLog = logs.find(log => log.req && log.res);
+        expect(requestLog).to.exist;
+        expect(requestLog.req.method).to.equal("GET");
+        expect(requestLog.req.url).to.equal("/test");
+        expect(requestLog.res.statusCode).to.equal(200);
+        expect(requestLog.responseTime).to.be.a("number");
     });
 });
